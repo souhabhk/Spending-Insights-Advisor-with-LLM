912e5ad25cf50775b0fc7ff9d95c735278a69c06
Souha Kacem <94676169+souhabhk@users.noreply.github.com>
2024-02-10 20:55:57 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+from db import *
+from langchain_experimental.sql import SQLDatabaseChain
+from model import llm
+from datetime import datetime
+import pytz
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    transactions = conn.execute('SELECT id, * FROM transactions ORDER BY date DESC').fetchall()
+    conn.close()
+    return render_template('index.html', transactions=transactions)
+
+# Creating the Prompt Template
+# LLaMA uses [INST] and [/INST] to indicate user messages, and <<SYS>> and <</SYS>> to indicate system messages
+QUERY = """
+<<SYS>> 
+You are a powerful text-to-SQLite model. Your job is to answer questions about a database. You are given a question and context regarding the table of credit card transaction records table which represents a person's expense records. 
+The table name is {table_name} and corresponding columns are {columns}.
+You must run SQLite queries to the table to find an answer. Ensure your query does not include any non-SQLite syntax such as DATE_TRUNC or any use of backticks (`) or "```sql". Then, execute this query against the 'transactions' table and provide the answer. 
+Provide strategies to manage expenses or tips to reduce the expenses in your answer as well. Compare the result with the spending in the previous months if any and include the insights in your answer.
+
+Guidelines:
+- Filter results using the current time zone: {time} only when query specifis a specific date/time period. You should use ">=" or "<=" operators to filter the date or use "GROUP BY strftime('%m', date)" for grouping into month.  Assume the date format in the database is 'YYYY-MM-DD'.
+- If the query result is [(None,)], run the SQLite query again to double check the answer. 
+- If a specific category is mentioned in the inquiry, such as 'Groceries', 'Dining', or 'Utilities', use the "WHERE" condition in your SQL query to filter transactions by that category. For example, when asked for the average amount spent on 'Groceries', use "SELECT AVG(amount) FROM transactions WHERE category = 'Groceries'".
+- If not asked for a specific category, yuou shouldn't filter any category out. On the other hand, you should use "where" condition to do the filtering. When asked for the average amount in a category, use the SQLite query (AVG(amount) WHERE category = 'category_name').
+- When asked for \'highest\' or \'lowest\', use SQL function MAX() or MIN() respectively.
+
+
+Use the following format to answer the inquiry:
+
+Response: Result of the SQLite-compatible SQL query. If you know th transaction detailes such as the date, category, merchant, and amount, mention it in your answer to be more clear. - When asked for an \'overview\' of transactions, analyze and present the data in a way that highlights the proportions of different categories or types of transactions. For instance, calculate the percentage each category (like \'Groceries\', \'Dining\', \'Utilities\') contributes to the total transactions or total spending.
+---------------------- line break
+<br>
+---------------------- line break
+<br>
+Explanation: Concise and succinct explanation on your thought process on how to get the final answer including the relevant transaction details such as the date, category, merchant, and amount.
+---------------------- line break
+<br>
+---------------------- line break
+<br>
+Advice: Provide strategies to manage expenses or tips to reduce the expenses here.
+
+<</SYS>>
+
+[INST] 
+{inquiry}
+[/INST]
+"""
+
+# Initializing the Database Chain
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+
+# Interacting with the Database
+@app.route('/inquiry', methods=['POST'])
+def submit_inquiry():
+    inquiry = request.form['inquiry']
+    # Handle transaction queries
+    prompt = QUERY.format(table_name=table_name, columns=columns, time=datetime.now(pytz.timezone('America/New_York')), inquiry=inquiry)
+    response = db_chain.run(prompt)
+    # Fetch transactions data again to pass to the template
+    conn = get_db_connection()
+    cursor = conn.execute('SELECT id, * FROM transactions ORDER BY date DESC')
+    transactions = [dict(ix) for ix in cursor.fetchall()]
+    conn.close()
+    # Replace newline characters with HTML break tags
+    response = response.replace('\n', '<br>')
+    
+    return render_template('index.html', inquiry=prompt, answer=response, transactions=transactions)
+
+# Running the App
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,50 @@
+
+from langchain.utilities import SQLDatabase
+import sqlite3
+import csv
+
+# Create our database
+db = SQLDatabase.from_uri("sqlite:///history.db")
+
+# Function to connect to SQLite database
+def get_db_connection():
+    conn = sqlite3.connect('history.db', check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+def get_table_columns(table_name):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA table_info({})".format(table_name))
+    columns = cursor.fetchall()
+    print(f"columns:{columns}")
+    return [column[1] for column in columns]
+
+table_name = 'transactions'
+columns = get_table_columns(table_name)
+def init_db():
+    conn = get_db_connection()
+    # Create a table
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date DATE,
+            category TEXT,
+            merchant TEXT,
+            amount INTEGER
+        );
+    """)
+    # Insert sample data from CSV file only if the table is empty
+    if conn.execute('SELECT COUNT(*) FROM transactions').fetchone()[0] == 0:
+        with open('transactions.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            sample_data = list(reader)
+            conn.executemany('INSERT INTO transactions (date, category, merchant, amount) VALUES (?, ?, ?, ?)', sample_data)
+        
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+init_db()
+
+print(db.table_info)
